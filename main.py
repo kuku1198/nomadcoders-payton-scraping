e0ff6f1089f22e2e6d5ebc1810654185078a2fcc
어영규 <kuku1198@gmail.com>
2021-09-20 18:19:05 +0900

    #1.7

---
+++ main.py
@@ -0,0 +1,11 @@
+def plus(a, b):
+    return a + b
+
+result = plus(b=30, a=1)
+print(result)
+
+def say_hello(name, age, are_form, fav_food):
+    return "Heelo " + name + " you are " + age + " years old"
+
+hello = say_hello("test", "31", "kimchi", "korea")
+print(hello)
