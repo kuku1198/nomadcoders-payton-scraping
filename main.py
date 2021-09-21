b9d941b8d69d7e48625f759f827b39625e8fab5b
어영규 <kuku1198@gmail.com>
2021-09-21 19:51:02 +0900

    #1.8

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
+def is_int_or_float(a, b=0):
+    return (type(a) is int or type(a) is float and type(b) is int or type(b) is float)
+
+
 def plus(a, b):
-    return a + b
+    if (is_int_or_float(a, b)):
+        return a + b
 
-result = plus(b=30, a=1)
-print(result)
+    return None
 
-def say_hello(name, age, are_form, fav_food):
-    return "Heelo " + name + " you are " + age + " years old"
 
-hello = say_hello("test", "31", "kimchi", "korea")
-print(hello)
+def minus(a, b):
+    if (is_int_or_float(a, b)):
+        return a - b
+
+    return None
+
+
+def times(a, b):
+    if (is_int_or_float(a, b)):
+        return a * b
+
+    return None
+
+
+def division(a, b):
+    if (is_int_or_float(a, b)):
+        return a / b
+
+    return None
+
+
+def negation(a):
+    if (is_int_or_float(a)):
+        return -a
+
+    return None
+
+
+def power(a, b):
+    if (is_int_or_float(a, b)):
+        return a ** b
+
+    return None
+
+
+def remainder(a, b):
+    if (is_int_or_float(a, b)):
+        return a % b
+
+    return None
+
+
+print(plus(3, 2))
+print(minus(3, 1))
+print(times(2, 2))
+print(division(9, 3))
+print(negation(3))
+print(power(10, 2))
+print(remainder(3, 9))
