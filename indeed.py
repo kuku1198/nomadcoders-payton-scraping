6e913a3aa88eb52c519d0ab88d90d10f5b40b063
어영규 <kuku1198@gmail.com>
2021-09-27 17:30:58 +0900

    #2.8

--- indeed.py
+++ indeed.py
@@ -20,32 +20,45 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_job(html):
+    titles = html.find("h2", {"class": "jobTitle"}).find_all("span")
+    title = titles[0].get('title')
+
+    if title is None:
+        title = titles[1].get('title')
+
+    company = html.find("span", {"class": "companyName"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company.strip()
+
+    location = html.find("div", {"class": "companyLocation"}).get_text()
+    job_id = html.find_parent("a")["data-jk"]
+
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        "link": f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
 
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0 * LIMIT}")
-    print(f"{URL}&start={0 * LIMIT}")
-    soup = BeautifulSoup(result.text, 'html.parser')
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page * LIMIT}")
+        soup = BeautifulSoup(result.text, 'html.parser')
 
-    job_card = soup.find(id="mosaic-provider-jobcards")
-    results = job_card.find_all("div", {"class": "job_seen_beacon"})
+        job_card = soup.find(id="mosaic-provider-jobcards")
+        results = job_card.find_all("div", {"class": "job_seen_beacon"})
 
-    for result in results:
-        titles = result.find("h2", {"class": "jobTitle"}).find_all("span")
-        title = titles[0].get('title')
-
-        if title is None:
-            title = titles[1].get('title')
-
-        company = result.find("span", {"class": "companyName"})
-        company_anchor = company.find("a")
-        if company_anchor is not None:
-            company = str(company_anchor.string)
-        else:
-            company = str(company.string)
-        company.strip()
-
-        print(title, company)
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
 
     return jobs
