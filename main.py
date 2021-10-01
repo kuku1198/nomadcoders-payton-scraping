df03d5dd253c285cd23c0c7e69426f4030a5fa12
어영규 <kuku1198@gmail.com>
2021-10-01 21:47:53 +0900

    #4.1

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-from indeed import get_jobs as get_indeed_jobs
-from stackoverflow import get_jobs as get_so_jobs
-from save import save_to_file
+from flask import Flask
+
+app = Flask("SuperScrapper")
 
 
-# indeed_jobs = get_indeed_jobs()
-# so_jobs = get_so_jobs()
-# jobs = indeed_jobs + so_jobs
-# save_to_file(jobs)
+@app.route("/")
+def home():
+    return "Hello World!"
 
 
-class Car:
-    def __init__(self, *args, **kwargs):
-        self.wheels = 4
-        self.doors = 4
-        self.windows = 4
-        self.seats = 4
-        self.color = kwargs.get("color", "black")
-        self.price = kwargs.get("price", "$20")
-
-    def __str__(self):
-        return f"Car with {self.wheels} wheels"
+@app.route("/contact")
+def contact():
+    return "Contact Me!"
 
 
-class Convertible(Car):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.time = kwargs.get("time", 10)
-
-    def take_oof(self):
-        return "taking off"
-
-    def __str__(self):
-        return f"Car with no roof"
-
-
-porche = Convertible(color="green", price="$40")
-print(porche.color)
+app.run()
