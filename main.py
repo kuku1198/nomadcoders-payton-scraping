611f66dc31e1c5d419013941d3ebe76e05751ef1
어영규 <kuku1198@gmail.com>
2021-10-02 21:08:41 +0900

    #4.3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("SuperScrapper")
 
@@ -6,6 +6,12 @@ app = Flask("SuperScrapper")
 @app.route("/")
 def home():
     return render_template("potato.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
 
 
 app.run()
